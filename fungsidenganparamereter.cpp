febee2fba2cce0e829b276d293bb7ccf85a7fb5f
Fadhil-sukamengaji <mudzakifadhil@gmail.com>
2026-03-02 20:57:20 +0700

    proses

--- fungsidenganparamereter.cpp
+++ fungsidenganparamereter.cpp
@@ -9,3 +9,9 @@ void input(){
     cout << "Masukkan lebar : ";
     cin >> lebar;
 }
+int luasPersegi(int a, int b){
+    return a * b;
+}
+int jumlah(int x, int y, int z){
+    return x + y + z;
+}
